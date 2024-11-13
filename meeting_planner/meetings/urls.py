e51cfa21ec1240976f80dad763ce8852d25560f3
hhussain <haarisah73@gmail.com>
2024-11-13 16:07:55 +0000

    Made web form for website 13/11/2024

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -4,4 +4,5 @@ from meetings import views
 urlpatterns = [
     path('<int:id>', views.detail,name="detail"),
     path('rooms', views.rooms_list, name ="rooms"),
+    path('new', views.new, name="new"),
 ]
